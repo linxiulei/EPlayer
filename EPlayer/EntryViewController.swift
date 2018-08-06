2486e8a6aa6d17331e045e0912ea9d1524c8d274
Eric Lin <linxiulei@gmail.com>
2018-08-06 19:40:30 +0800

    storyboard: Use ContainerView to layout entry view
    Use ContainerView to layout entry view, which contains two view 1)
    FileListView and ControlPanelView. The latter one would be used to
    enable some functional features in the future.

---
+++ EPlayer/EntryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EntryViewController.swift
+//  EPlayer
+//
+//  Created by 林守磊 on 2018/8/6.
+//  Copyright © 2018 林守磊. All rights reserved.
+//
+
+import Foundation
+
+class EntryViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
