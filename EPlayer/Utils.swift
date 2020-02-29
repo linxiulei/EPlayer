56afa7037f0275e849ddb5e2d152043e36237b35
Eric Lin <linxiulei@gmail.com>
2020-02-29 16:06:18 +0000

    Utils: Support guessing filenames with fields separated by underscore

--- EPlayer/Utils.swift
+++ EPlayer/Utils.swift
@@ -403,8 +403,9 @@ class MovieGuesser {
             Silicon.Valley.S01E01.720p.BluRay.x265.ShAaNiG
             Love, Death & Robots - S01E01 - Sonnie's Edge
             CaptainMarvel.2019.1080p.WEB-DL.H264.AC3-EVO
+            friends_s01e01_720p_bluray_264-fdaf.mkv
         */
-        let pattern = "([\\w. &,]+)[- ]*([sS]\\d+[eE]\\d+|\\d{4})[\\. -]+.*"
+        let pattern = "([\\w. &,]+)[-_ ]*([sS]\\d+[eE]\\d+|\\d{4})[\\. -_]+.*"
 
         let regex = try! NSRegularExpression(pattern: pattern,
                                                  options: [])
